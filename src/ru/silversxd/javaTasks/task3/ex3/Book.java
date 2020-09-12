f2955b194e86fe27fb61a08c085a3847af786e2c
SilversxD <silverscjserge@gmail.com>
2020-09-12 15:21:23 +0300

    Fifth commit

--- src/ru/silversxd/javaTasks/task3/ex3/Book.java
+++ src/ru/silversxd/javaTasks/task3/ex3/Book.java
@@ -1,4 +1,38 @@
 package ru.silversxd.javaTasks.task3.ex3;
 
+
 public class Book {
+    private String author;
+    private String date;
+    private String name;
+    public Book(String author, String date, String name){
+        this.author=author;
+        this.date=date;
+        this.name=name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/ru/silversxd/javaTasks/task3/ex3/testBook.java
+++ src/ru/silversxd/javaTasks/task3/ex3/testBook.java
@@ -1,4 +1,12 @@
 package ru.silversxd.javaTasks.task3.ex3;
 
 public class testBook {
+    public static void main(String[] args) {
+        Book book = new Book("Pushkin","12 february 1998","451");
+        book.setAuthor("Shexpear");
+        book.setDate("12 september 2018");
+        book.setName("Gamlet");
+        System.out.println(book.getAuthor()+" "+book.getDate()+" "+book.getName());
+
+    }
 }
