99c3b4ac54d462479e2be4b8a21c056ea7d53298
SilversxD <silverscjserge@gmail.com>
2020-10-08 13:27:42 +0300

    Micro fix in 9 lab

--- src/ru/silversxd/javaTasks/task9/Button.java
+++ src/ru/silversxd/javaTasks/task9/Button.java
@@ -1,14 +1,30 @@
 package ru.silversxd.javaTasks.task9;
-import ru.silversxd.javaTasks.task8.Shapes.Circle.*;
-import ru.silversxd.javaTasks.task8.Shapes.Rectangle.*;
-import ru.silversxd.javaTasks.task8.Shapes.Square.*;
+import ru.silversxd.javaTasks.task8.Shapes.*;
 
-public class Button {
-    button.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            re();
-            paintComponent(g);
-        }
+import javax.swing.*;
+import java.awt.*;
 
+public class Button extends JFrame {
+
+    private final int windowWidth = 800;
+    private final int windowHeight = 600;
+
+    public Button()
+    {
+        super("Generate Shape by Button");
+
+        JButton b = new JButton("Add Random Shape");
+        RandomShapesTest shape = new RandomShapesTest();
+
+        setLayout(new FlowLayout());
+        setSize(windowWidth, windowHeight);
+        add(b);
+        b.addActionListener(e -> shape.paint(getGraphics()));
+        setVisible(true);
     }
+
+    public static void main(String[] args) {
+        new Button();
+    }
+
+}
